930333bea6f175671394722593e61bd5a3ad7dfd
Nitin Bargaje <nitinbargaje7@gmail.com>
2024-12-28 14:29:55 +0530

    First cmake project

---
+++ Ep002/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b) {
+    return a + b;
+}
+int main() {
+    std::cout << "Hello World!";
+    std::cout << add(3.5 + 7.2);
+}
